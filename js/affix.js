d49dcc0e879e80fda5b9d0b0525b64f572d8247d
yupzhou <yupzhou@foxmail.com>
2019-11-05 23:58:47 +0800

    initial commit

---
+++ js/affix.js
@@ -0,0 +1,523 @@
+var affixdata =  [
+      {
+        "title": "",
+        "class": "1",
+        "summary": "",
+        "content": "<b>[元音] </b>a, i (i1), ui/əi (i2), u, ia(e1), ai(e2), ua(o1), ə(o2)</br><b>[牙音] </b>k, nk</br><b>[齒音] </b>s, ns</br><b>[舌音] </b>t, nt, n</br><b>[脣音] </b>p, mp, m</br><b>[半元音・流音] </b>d-/-j-, r, b-/-w-"
+      },
+	{
+"title":"參照",
+"class":"1",
+"summary":"",
+"content":"<b>[日本祖語的構擬] </b>2003 S. A. Starostin, A. V. Dybo, O. A. Mudrak (with the assistance of I. Gruntov and V. Glumov) An Etymological Dictionary of Altaic Languages<br><b>[日語參考詞典] </b>1939-1940 正宗敦夫 <a href='http://dl.ndl.go.jp/info:ndljp/pid/1212353' target='_blank'>類聚名義抄. 仮名索引ア～カ</a>、2008 株式会社岩波書店 広辞苑 第六版 DVD-ROM版<br><b>[琉球方言] </b>1999-2003 Okinawa Center of Language Study <a href='http://ryukyu-lang.lib.u-ryukyu.ac.jp/index.html' target='_blank'>琉球語音声データベース</a><br><b>[南島語] </b>2010-2018 Robert Blust, Stephen Trussel <a href='http://www.trussel2.com/acd/' target='_blank'>The Austronesian Comparative Dictionary web edition</a><br><b>[朝鮮語] </b>2002-2005 <a href='http://www.zonmal.com/' target='_blank'>존 한자사전</a><br><b>[高句麗語] </b>2004 Chistopher I. Beckwith Koguryo the Language of Japan's Continental Relatices<br><b>[古音數據庫] </b>2012 郭家寶 <a href='http://ytenx.org/' target='_blank'>韻典網</a>、</b>2018 Jerry <a href='http://www.ccamc.co/index.php' target='_blank'>古今文字集成</a>、</b>2003 鄭張尚芳 上古音系、</b>2014 William H. Baxter and Laurent Sagart <a href='http://ocbaxtersagart.lsait.lsa.umich.edu/' target='_blank'>The Baxter-Sagart reconstruction of Old Chinese (Version 1.1, 20 September 2014)</a><br><b>[其他] </b><a href='https://ja.wikipedia.org/' target='_blank'>ウィキペディア</a>、<a href='https://ja.wiktionary.org/' target='_blank'>ウィクショナリー</a>"
+},
+
+	 //例子
+	 /*{
+        "title": "ア",
+        "class": "2",
+        "summary": "得",
+        "content": "<span class='heresearch'>ウ(エル)</span> (a)-u -u vt. 2a <spam class='heresearch'>得 能</spam><br>"
+      },*/
+
+	/**/{
+        "title": "ア",
+        "class": "2",
+        "summary": "感動",
+        "content": "└[interj] <span class='heresearch'>ア</span> [感動, 呼喚, 應答, 驚訝]<br>　├[interj] <span class='heresearch'>アア</span> a-a [感動, 呼喚, 應答] <spam class='heresearch'>嗚呼 噫</spam> [鳥鳴] 啞啞<br>　├[interj] <span class='heresearch'>アナ</span> a-na [感動]<br>　├┬┬[interj] <span class='heresearch'>アナニヱヤ(アナニエヤ) </span> a-na-ni(*-nV形容動詞連用)-we-ja(感動) [對美的感嘆] <spam class='heresearch'>美哉 妍哉</spam><br>　││└ [interj] <span class='heresearch'>アナニヤシ</span> a-na-ni(*-nV形容動詞連用)-ja-si(*-sV強意) [對美的感嘆] <spam class='heresearch'>美哉</spam><br>　│└[interj] <span class='heresearch'>アナヤ</span> a-na-ja [驚訝]<br>　├[interj] <span class='heresearch'>アヤ</span> a-ja [感動] <br>　└[interj] <span class='heresearch'>アラ</span> a-ra [感動] <br>"
+      },
+
+      {
+        "title": "ア",
+        "class": "2",
+        "summary": "得",
+        "content": "<span class='heresearch'>ウ(エル)</span> (a)-u -u vt. 2a <span class='heresearch'>得 能</span>"
+      },
+      {
+        "title": "ア",
+        "class": "2",
+        "summary": "能",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "ア",
+        "class": "2",
+        "summary": "第一人称間格",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "ア",
+        "class": "2",
+        "summary": "遠指・第三人称",
+        "content": "*é that (deictic root) > *á- that<br>ア a<br>アレ a-re *-rV<br><b>對應朝鮮語</b> PKor. *ā-mò someone, some > MKor. āmò (cf. also ə̀nɨ́ ‘some, someone’, àrắm ‘oneself’) > Mod. āmu"
+      },
+      {
+        "title": "アカ",
+        "class": "2",
+        "summary": "垢",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "アカ",
+        "class": "2",
+        "summary": "開",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+
+
+
+
+      {
+        "title": "イ",
+        "class": "3",
+        "summary": "第二人称 ",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "イ",
+        "class": "3",
+        "summary": "射",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "イ",
+        "class": "3",
+        "summary": "斎",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+
+
+
+
+
+      {
+        "title": "ウ",
+        "class": "4",
+        "summary": "鵜",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "ウ",
+        "class": "4",
+        "summary": "浦",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "ウ",
+        "class": "4",
+        "summary": "上",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+
+
+
+
+
+      {
+        "title": "エ",
+        "class": "5",
+        "summary": "江",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "エ",
+        "class": "5",
+        "summary": "疫",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "エ",
+        "class": "5",
+        "summary": "選",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+
+
+
+
+
+
+
+      {
+        "title": "オ",
+        "class": "6",
+        "summary": "第二人称",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "オ",
+        "class": "6",
+        "summary": "穏",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "オ",
+        "class": "6",
+        "summary": "帯",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+
+
+
+
+
+      {
+        "title": "カ",
+        "class": "7",
+        "summary": "母",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "カ",
+        "class": "7",
+        "summary": "日",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "カ",
+        "class": "7",
+        "summary": "蚊",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+
+
+
+
+
+      {
+        "title": "キ",
+        "class": "8",
+        "summary": "刻",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "キ",
+        "class": "8",
+        "summary": "葱",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "キ",
+        "class": "8",
+        "summary": "城",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+
+
+
+
+      {
+        "title": "ク",
+        "class": "9",
+        "summary": "黄",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "ク",
+        "class": "9",
+        "summary": "岫",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "ク",
+        "class": "9",
+        "summary": "茎",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+
+
+
+
+
+      {
+        "title": "ケ",
+        "class": "10",
+        "summary": "異",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "ケ",
+        "class": "10",
+        "summary": "毛",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "ケ",
+        "class": "10",
+        "summary": "獣",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+
+
+	{
+        "title": "コ",
+        "class": "11",
+        "summary": "木〈南島語〉",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "コ",
+        "class": "11",
+        "summary": "来",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+      {
+        "title": "コ",
+        "class": "11",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  
+      {
+        "title": "コ",
+        "class": "12",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+
+	  {
+        "title": "コ",
+        "class": "13",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "14",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "15",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "16",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "17",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+
+	  {
+        "title": "コ",
+        "class": "18",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      }, 
+	  
+	  {
+        "title": "コ",
+        "class": "19",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "20",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+
+	  {
+        "title": "コ",
+        "class": "21",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "22",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      }, 
+	  
+	  {
+        "title": "コ",
+        "class": "23",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "24",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "25",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      }, 
+	  
+	  {
+        "title": "コ",
+        "class": "26",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "27",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+
+	  {
+        "title": "コ",
+        "class": "28",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "29",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "30",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "31",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "32",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "33",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+
+	  {
+        "title": "コ",
+        "class": "34",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "35",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "36",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },
+	  
+	  {
+        "title": "コ",
+        "class": "37",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },	  
+	  
+	  	  {
+        "title": "コ",
+        "class": "38",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },	  
+	  	  {
+        "title": "コ",
+        "class": "39",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },	  
+	  	  {
+        "title": "コ",
+        "class": "40",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },	  
+	  	  {
+        "title": "コ",
+        "class": "41",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },	  
+	  	  {
+        "title": "コ",
+        "class": "42",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },	  
+	  	  {
+        "title": "コ",
+        "class": "43",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },	  
+	  	  {
+        "title": "コ",
+        "class": "44",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },	  
+	  	  {
+        "title": "コ",
+        "class": "45",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },	  
+	  	  {
+        "title": "コ",
+        "class": "46",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },	  
+	  	  {
+        "title": "コ",
+        "class": "47",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },	  
+	  	  {
+        "title": "コ",
+        "class": "48",
+        "summary": "恋",
+        "content": "アク /aka-u/vt.2b>1b/開<br/>アク /ak-/*-ji-/vi.4>5/開"
+      },	  
+  
+
+	  
+  ];
