6974fce3cda55d3423f2a3edab9402125f8b3531
yupzhou <yupzhou@foxmail.com>
2019-11-23 23:29:34 +0800

    191123, 2.3

--- js/root.js
+++ js/root.js
@@ -24,7 +24,8 @@ var rootdata =  [
         "title": "イ",
         "class": "2",
         "summary": "i　鋳",
-        "content": "<b>i-r-u</b>\
+        "content": "❀ PA *<i>dĳV ( ~ *t-)</i>, PK *<i>tī-</i><br>\
+		<b>i-r-u</b>\
 		<span class='heresearch'><b>イル</b></span>\
 		<b><sup>EMJ</sup> vt 鋳</b><br>\
 		　　铸造。<br>\
@@ -35,7 +36,8 @@ var rootdata =  [
 		    "title": "イ",
 		    "class": "2",
 		    "summary": "i　胆",
-		    "content": "<b>i</b>\
+		    "content": "❀ PA *<i>ti̯ṓle</i>, PK *<i>tira ( ~ č-)</i><br>\
+			<b>i</b>\
 		<span class='heresearch'><b>イ</b></span>\
 		<b><sup>EMJ</sup> vt 胆</b><br>\
 		　　胆囊。<br>\
@@ -45,7 +47,8 @@ var rootdata =  [
 		    "title": "イカ",
 		    "class": "2",
 		    "summary": "ika　紙鳶",
-		    "content": "<b>ika</b>\
+		    "content": "❀ PA *<i>d[é]gì</i><br>\
+			<b>ika</b>\
 		<span class='heresearch'><b>イカ</b></span>\
 		<b><sup>EMnJ</sup> n 紙鳶</b><br>\
 		　　风筝。<br>\
@@ -57,64 +60,146 @@ var rootdata =  [
 		"
 		  },	
 		{
-		    "title": "イ",
+		    "title": "イカ",
 		    "class": "2",
-		    "summary": "1",
-		    "content": "<span class='heresearch'>イ</span>"
+		    "summary": "ika　筏",
+		    "content": "❀ PA *<i>télki</i>, PK *<i>tɨ̀rkuə̀r</i><br>\
+		<b>ika-nta</b>\
+		<span class='heresearch'><b>イカダ</b></span>\
+		<b><sup>WOJ</sup> n 筏</b><br>\
+		　　木筏。<br>\
+		　　-nta 名词后缀。<br>\
+		"
 		  },	
 		{
-		    "title": "イ",
+		    "title": "イガ",
 		    "class": "2",
-		    "summary": "1",
-		    "content": "<span class='heresearch'>イ</span>"
+		    "summary": "inka　毬",
+		    "content": "❀ PA *<i>dḕgni</i><br>\
+		<b>inka</b>\
+		<span class='heresearch'><b>イガ</b></span>\
+		<b><sup>EMJ</sup> n 毬</b><br>\
+		　　刺球，带刺外壳。<br>\
+		"
+		  },		
+		{
+		    "title": "イク",
+		    "class": "2",
+		    "summary": "iku　軍",
+		    "content": "❀ PA *<i>dằgì</i>, PK *<i>tōi</i><br>\
+		<b>iku-sa</b>\
+		<span class='heresearch'><b>イクサ</b></span>\
+		<b><sup>WOJ</sup> n 軍</b><br>\
+		　　士兵，军队；战争。<br>\
+		　　-sa 名词后缀。<br>\
+		"
+		  },
+		{
+		    "title": "イハ",
+		    "class": "2",
+		    "summary": "ipa　岩",
+		    "content": "❀ PA *<i>tújpè ( ~ d-)</i><br>\
+		<b>ipa</b>\
+		<span class='heresearch'><b>イハ</b></span><b><sup>WOJ</sup>, </b>  \
+		<span class='heresearch'><b>イワ</b></span><b><sup>MnJ</sup> \
+		n 岩</b><br>\
+		　　岩石。<br>\
+		"
 		  },	
 		{
-		    "title": "イ",
+		    "title": "イヒ",
 		    "class": "2",
-		    "summary": "1",
-		    "content": "<span class='heresearch'>イ</span>"
+		    "summary": "ipi　家",
+		    "content": "❀ PA *<i>ǯī̀bì</i>, PK *<i>čìp</i><br>\
+		<b>ipi-a</b>\
+		<span class='heresearch'><b>イへ</b></span><b><sup>WOJ</sup>,  </b>\
+		 <span class='heresearch'><b>イハ</b></span><b><sup>EOJ</sup>,  </b>\
+		 <span class='heresearch'><b>イエ</b></span><b><sup>MnJ</sup>\
+		 n 軍</b><br>\
+		　　住宅；家庭，家世。<br>\
+		　　-a < -Ca 名词后缀。<br>\
+		"
 		  },	
 		{
-		    "title": "イ",
+		    "title": "イビ",
 		    "class": "2",
-		    "summary": "1",
-		    "content": "<span class='heresearch'>イ</span>"
+		    "summary": "impi　苛",
+		    "content": "❀ PA *<i>ǯḕri</i><br>\
+		<b>impi-r-</b>\
+		<span class='heresearch'><b>イビル</b></span>\
+		<b><sup>MnJ</sup> vt</b><br>\
+		　　挖苦；焙烧。<br>\
+		　　-r 他动词后缀。<br>\
+		"
 		  },	
 		{
-		    "title": "イ",
+		    "title": "イブ",
 		    "class": "2",
-		    "summary": "1",
-		    "content": "<span class='heresearch'>イ</span>"
+		    "summary": "impu　燻",
+		    "content": "❀ PA *<i>ǯip`o</i><br>\
+		<b>impi-s- </b><span class='heresearch'><b>イブス</b></span><b><sup>LMJ</sup> vt 燻</b><br>\
+		　　（烟等）熏；挖苦。<br>　　-s 他动词后缀。<br>\
+		<b>impi-r- </b><span class='heresearch'><b>イブル</b></span><b><sup>LMJ</sup> vi 燻</b><br>\
+		　　冒烟；呛。<br>　　-r 自动词后缀。<br>\
+		"
 		  },	
 		{
-		    "title": "イ",
+		    "title": "イル",
 		    "class": "2",
-		    "summary": "1",
-		    "content": "<span class='heresearch'>イ</span>"
+		    "summary": "ir　入",
+		    "content": "❀ PA *<i>tire</i>, PK *<i>tɨ́r-</i><br>\
+		<b>ir- </b><span class='heresearch'><b>イル</b></span><b><sup>WOJ</sup> vt 入</b><br>\
+		　　进入。<br>\
+		<b>ir-a- </b><span class='heresearch'><b>イル</b></span><b><sup>WOJ</sup> vi 入</b><br>\
+		　　放入，缴纳，容纳，包含，添加，雇。<br>　　-a 自他转换后缀。<br>\
+		"
 		  },	
 		{
-		    "title": "イ",
-		    "class": "2",
-		    "summary": "1",
-		    "content": "<span class='heresearch'>イ</span>"
+		    "title": "エリ",
+		    "class": "4",
+		    "summary": "jeri　襟",
+		    "content": "LMJ时e已无甲乙之分, 合流为je。<br>\
+			❀ PA *<i>dḗlì</i><br>\
+			<b>jeri </b><span class='heresearch'><b>エリ</b></span><b><sup>LMJ</sup> n 襟</b><br>\
+			　　衣襟。<br>\
+			"
 		  },	
 		{
-		    "title": "イ",
-		    "class": "2",
-		    "summary": "1",
-		    "content": "<span class='heresearch'>イ</span>"
-		  },	
-		{
-		    "title": "イ",
-		    "class": "2",
-		    "summary": "1",
-		    "content": "<span class='heresearch'>イ</span>"
-		  },	
-		{
-		    "title": "イ",
-		    "class": "2",
-		    "summary": "1",
-		    "content": "<span class='heresearch'>イ</span>"
+		    "title": "コ",
+		    "class": "10",
+		    "summary": "ko　此,　k[o]-a　彼",
+		    "content": "❀ PA *<i>ko ( ~ *k`-)</i>, PK *<i>kɨ</i><br>\
+			<b>ko </b><span class='heresearch'><b>コ</b></span><b><sup>WOJ</sup> pron 此 是</b><br>\
+			　　近指示，此，这。<br>\
+			<b>ko-ko </b><span class='heresearch'><b>ココ</b></span><b><sup>WOJ</sup> pron 此処</b><br>\
+			　　近处，此处，这里。<br>　　-ko 场所。<br>\
+			<b>ko-ti </b><span class='heresearch'><b>コチ</b></span><b><sup>WOJ</sup> pron 此方</b><br>\
+			　　近处的人/物；我。<br>　　-ti 方向。<br>\
+			<b>ko-n[o-k]ata </b><span class='heresearch'><b>コナタ</b></span><b><sup>WOJ</sup> pron 此方</b><br>\
+			　　近处的人/物；这位，您，我。<br>　　-n[o-k]ata对人表示尊敬。<br>\
+			<b>ko-ja-tu </b><span class='heresearch'><b>コヤツ</b></span><b><sup>EMJ</sup>, </b>\
+			<span class='heresearch'><b>クヤツ</b></span><b><sup>EMJV</sup>, </b>\
+			<span class='heresearch'><b>コイツ</b></span><b><sup>LMJV</sup>\
+			 pron (sc) 此奴</b><br>\
+			　　（蔑）这家伙。<br>　　-ja-tu 对人表示轻蔑。<br>\
+			<b>ko-re </b><span class='heresearch'><b>コレ</b></span><b><sup>WOJ</sup> pron 此</b><br>\
+			　　近指示，此，这。<br>　　-re 代词后缀。<br>\
+			<b>k[o]-a </b><span class='heresearch'><b>カ</b></span><b><sup>WOJ</sup> pron 彼</b><br>\
+			　　远指示，彼，那。<br>　　-a 远指示。<br>\
+			<b>k[o]-a-si-ko </b><span class='heresearch'><b>カシコ</b></span><b><sup>EMJ</sup> pron 彼処</b><br>\
+			　　远处，那里。<br>　　-si = -so 中指示。<br>　　-ko 场所。<br>\
+			<b>k[o]-a-n[o-k]ata </b><span class='heresearch'><b>カナタ</b></span><b><sup>WOJ</sup> pron 彼方</b><br>\
+			　　远处的人/物；您。<br>　　-n[o-k]ata 对人表示尊敬。<br>\
+			<b>k[o]-a-ja-tu </b><span class='heresearch'><b>カヤツ</b></span><b><sup>EMJ</sup>\
+			 pron (sc) 彼奴</b><br>\
+			　　（蔑）那家伙。<br>　　-ja-tu 对人表示轻蔑。<br>\
+			<b>k[o]-a-re </b><span class='heresearch'><b>カレ</b></span><b><sup>WOJ</sup> pron 彼</b><br>\
+			　　远指示，彼，那。<br>　　-re 代词后缀。<br>\
+			<b>k[o]-a-re-n-ka-si </b><span class='heresearch'><b>カレガシ</b></span><b><sup>LMJ</sup></b>, \
+			<span class='heresearch'><b>カガシ</b></span><b><sup>LMJV</sup>\
+			 pron 彼某</b><br>\
+			　　那个谁。<br>\
+			"
 		  },	
 		{
 		    "title": "イ",
