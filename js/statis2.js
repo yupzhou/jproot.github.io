d1a827ad9b0d1352ca7419743dd020ae74a715c0
yupzhou <yupzhou@foxmail.com>
2020-03-11 23:11:38 +0800

    200311, 2.8 -- accent

---
+++ js/statis2.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?944a85a8e59a224272826ab9c9130a98";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
---
+++ js/statis3.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?595afa6c72b7b013c5ea82e33a7530de";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
